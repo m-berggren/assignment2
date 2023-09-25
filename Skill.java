02954a7a99f943e20fc0baa69e861230ad87b517
marcusberggren <berggren.me@outlook.com>
2023-09-25 21:49:17 +0200

    task 1 fully working

--- Skill.java
+++ Skill.java
@@ -38,7 +38,7 @@ public class Skill {
             isEqual = true;
         } else if (anotherObject == null) {
             isEqual = false;
-        } else {
+        } else if(anotherObject instanceof Skill) {
             Skill anotherSkill = (Skill) anotherObject;
             boolean sameName = this.nameOfSkill.equals(anotherSkill.nameOfSkill);
             boolean sameAP = this.attackPower == anotherSkill.getAttackPower();
